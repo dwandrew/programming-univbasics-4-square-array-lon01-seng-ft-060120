f4cbb1caa5e0d76b4c26b26299b0840b2162b9ad
Daniel Andrew <dwandrew@hotmail.com>
2020-04-03 13:19:26 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,17 +1,14 @@
 
 def square_array(array)
   i=0;
-<<<<<<< HEAD
   squared_array = []
   while i< array.length do
   squared_array.push(array[i]*array[i])
   i+=1
   end
   return squared_array
-=======
   while i< array.length do
   square_array.push(array[i]*array[i])
   i+=1
   end
->>>>>>> 62b9dee0cf89067a027a33a01c2b3100566f9853
 end
